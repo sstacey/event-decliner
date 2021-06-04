51eede78865cbe457bdf446aee655aa74e0e2cdc
sstacey <shaun.stacey1@gmail.com>
2021-06-04 08:51:20 -0500

    Finish project

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
-// write program here
+/* 
+Need to turn down an event?  
+We've got you covered.  
+Just run this program to generate a random excuse!
+*/
 
+const fs = require('fs')
 
-const getJokes = (fileName) => {
-    const fs = require('fs')
-
-    const textArr = fs.readFile(fileName, 'utf8', (err, data) => {
-        if (err) {
-            throw err
-        }
-        return data.split('\n')
-    })
-    console.log(textArr)
-    console.log(textArr)
-
-    
-
-    // fs.readFile(filename, 'utf8', function(err, data) {
-    // if (err) throw err;
-    // console.log('OK: ' + filename);
-    // console.log(data)
+const getJoke = () => {
+    const fileName = 'jokes.txt'
+    // Read in raw jokes text
+    const jokesText = fs.readFileSync(fileName, 'utf8')
+    // Remove padding and number from jokes
+    const jokesArr = jokesText.split('\n').map((joke) => joke.substring(7))
+    // Geerate random number for random joke
+    randNum = Math.floor(Math.random() * jokesArr.length)
+    return `
+        Random Excuse:
+        I'd love to, but ${jokesArr[randNum]}
+    `
 }
 
-
-getJokes('jokes.txt')
+console.log(getJoke())
