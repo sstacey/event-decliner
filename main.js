ac89dd31daf02ec39cca7ae31f1daa6c8c018d67
TheTC923 <77299532+TheTC923@users.noreply.github.com>
2021-06-04 21:24:38 +0200

    Split in two functions for easy maintenance

--- main.js
+++ main.js
@@ -4,20 +4,28 @@ We've got you covered.
 Just run this program to generate a random excuse!
 */
 
-const fs = require('fs')
+// Import fs module
+const fs = require('fs');
 
-const getJoke = () => {
-    const fileName = 'jokes.txt'
+const fileName = 'jokes.txt';
+
+const getFile = fileName => {
     // Read in raw jokes text
     const jokesText = fs.readFileSync(fileName, 'utf8')
     // Remove padding and number from jokes
-    const jokesArr = jokesText.split('\n').map((joke) => joke.substring(7))
+    return jokesText.split('\n').map((joke) => joke.substring(7))
+    
+}
+
+const getJoke = (jokesArr) => {
     // Geerate random number for random joke
     randNum = Math.floor(Math.random() * jokesArr.length)
+    // Return random excuse
     return `
         Random Excuse:
         I'd love to, but ${jokesArr[randNum]}
     `
 }
 
-console.log(getJoke())
+const jokesArr = getFile(fileName);
+console.log(getJoke(jokesArr))
