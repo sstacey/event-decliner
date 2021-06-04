8997dadde67dcfe2700b732636ba09aef28869df
sstacey <shaun.stacey1@gmail.com>
2021-06-04 08:23:49 -0500

    Start work

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 // write program here
+
+
+const getJokes = (fileName) => {
+    const fs = require('fs')
+
+    const textArr = fs.readFile(fileName, 'utf8', (err, data) => {
+        if (err) {
+            throw err
+        }
+        return data.split('\n')
+    })
+    console.log(textArr)
+    console.log(textArr)
+
+    
+
+    // fs.readFile(filename, 'utf8', function(err, data) {
+    // if (err) throw err;
+    // console.log('OK: ' + filename);
+    // console.log(data)
+}
+
+
+getJokes('jokes.txt')
